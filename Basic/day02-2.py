8909263c132f1dbcb276a5c0944b5416256800c9
comely0120 <65273243+comely0120@users.noreply.github.com>
2021-01-26 16:57:44 +0900

    Add files via upload

---
+++ Basic/day02-2.py
@@ -0,0 +1,44 @@
+""" 파이썬 데이터 타입 종류
+1.Boolean : 1 TRUE, 0 FALSE
+2.Numbers
+3.String
+4.Bytes
+5.Lists : ❤❤
+6.Tuples : 
+7.Sets : 집합
+8.Dictionaries : ❤❤
+"""
+
+dict1 = {
+    "name" : "Comely",
+    "age" : 23
+} 
+list1 = [1,2,3,4]
+set1 = {5,6,7}
+print(type(dict1))
+
+
+i1= 5
+f1=31.123456789
+result1 = i1 + f1
+result2 = i1 * f1
+result3 = i1 ** f1
+print(result1,type(result1)) # float
+print(result2,type(result2)) # float
+print(result3,type(result3)) # float
+
+# 형변환 int, float, complex(복소수)
+print(int(result1))
+print(complex(3)) # (3+0j)
+print(complex(0)) # 0j
+print(int('3')) # 3
+
+# divmod : 몫은 n, 나머지는 m
+n,m = divmod(100,8)
+print(n,m)
+
+# math.ceil : 현재 숫자보다 크면서 가장 작은 정수
+# math.ceil : 현재 숫자보다 작으면서 가장 큰 정수
+import math
+print(math.ceil(5.1)) #6
+print(math.floor(5.1)) #5
